459db109e41be8a6de14d712d8ea1916f930f7ac
Cheng Tsai <tsai_cheng@yahoo.comgit config --global user.email tsai_cheng@yahoo.com>
2018-03-02 17:01:45 -0800

    Work on active record lite

--- active_record_lite/lib/01_sql_object.rb
+++ active_record_lite/lib/01_sql_object.rb
@@ -5,46 +5,82 @@ require 'active_support/inflector'
 
 class SQLObject
   def self.columns
-    # ...
+    return @column if @column
+
+    cols = DBConnection.execute2(<<-SQL)
+      SELECT
+        *
+      FROM
+        "#{self.table_name}"
+      LIMIT
+        0
+    SQL
+
+    cols[0].map!(&:to_sym)
+    @column = cols[0]
   end
 
   def self.finalize!
+    self.columns.each do |name|
+      define_method(name) do
+        self.attributes[name]
+      end
+
+      define_method("#{name}=") do |value|
+        self.attributes[name] = value
+      end
+    end
   end
 
   def self.table_name=(table_name)
-    # ...
+    @table_name = table_name
   end
 
   def self.table_name
-    # ...
+    "#{self.to_s.downcase}s"
   end
 
   def self.all
-    # ...
+    table = DBConnection.execute2(<<-SQL)
+      SELECT
+        *
+      FROM
+        "#{self.table_name}"
+    SQL
+    table[1..-1]
   end
 
   def self.parse_all(results)
-    # ...
+
   end
 
   def self.find(id)
-    # ...
+    table = DBConnection.execute2(<<-SQL)
+      SELECT
+        *
+      FROM
+        "#{self.table_name}"
+    SQL
+
+    table[1+id]
   end
 
   def initialize(params = {})
-    # ...
+
   end
 
   def attributes
-    # ...
+    @attributes ||= {}
   end
 
   def attribute_values
-    # ...
+    
   end
 
   def insert
-    # ...
+    DBConnection.execute2(<<-SQL)
+
+    SQL
   end
 
   def update
